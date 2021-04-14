349c12b0967eb0ba87a8e49142f69c1aa5f3eb79
Jordan Holland <jordan.mark.holland@gmail.com>
2021-04-14 14:24:53 +1000

    Add Note class.

---
+++ lib/src/models/note.dart
@@ -0,0 +1,46 @@
+import 'package:numerote_core/src/models/label.dart';
+import 'package:uuid/uuid.dart';
+
+class Note {
+  Note({
+    String? documentId,
+    required this.contents,
+    List<Label>? labels,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  })  : documentId = documentId ?? const Uuid().v4(),
+        labels = labels ?? [],
+        createdAt = createdAt ?? DateTime.now(),
+        updatedAt = updatedAt ?? DateTime.now();
+
+  final String documentId;
+  final String contents;
+  final List<Label> labels;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  factory Note.createNew() => Note(contents: "");
+
+  @override
+  bool operator ==(Object other) =>
+      other is Note && other.documentId == documentId;
+
+  @override
+  int get hashCode => documentId.hashCode;
+
+  Note copyWith({
+    String? documentId,
+    String? contents,
+    List<Label>? labels,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) {
+    return Note(
+      documentId: documentId ?? this.documentId,
+      contents: contents ?? this.contents,
+      labels: labels ?? this.labels,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
+}
