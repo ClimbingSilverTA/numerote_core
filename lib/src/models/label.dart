f49a61333096e6b00a3c1077f431560662014d1e
Jordan Holland <jordan.mark.holland@gmail.com>
2021-04-14 14:24:01 +1000

    Add Label class.

---
+++ lib/src/models/label.dart
@@ -0,0 +1,23 @@
+import 'package:uuid/uuid.dart';
+
+class Label {
+  Label({
+    String? documentId,
+    required this.name,
+    DateTime? createdAt,
+  })  : documentId = documentId ?? const Uuid().v4(),
+        createdAt = createdAt ?? DateTime.now();
+
+  final String documentId;
+  final String name;
+  final DateTime createdAt;
+
+  factory Label.create({required String name}) => Label(name: name);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Label && other.documentId == documentId;
+
+  @override
+  int get hashCode => documentId.hashCode;
+}
--- test/numerote_core_test.dart
+++ test/numerote_core_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:numerote_core/numerote_core.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('numerote_core');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await NumeroteCore.platformVersion, '42');
   });
 }
