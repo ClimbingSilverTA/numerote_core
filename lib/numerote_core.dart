4b10d8fb1dc6866b68def911e6e02a75468a7a36
Jordan Holland <jordan.mark.holland@gmail.com>
2021-04-25 10:50:04 +1000

    Add some comments.

--- lib/numerote_core.dart
+++ lib/numerote_core.dart
@@ -18,14 +18,18 @@ class NumeroteCore {
         notes = CoreRepo<Note>(adapter: adapter),
         labels = CoreRepo<Label>(adapter: adapter);
 
+  /// Creates an instance in memory, so it doesn't persist between sessions
   factory NumeroteCore.inMemory() =>
       NumeroteCore._internal(adapter: NotesMemoryAdapter());
 
+  /// Creates an instance that uses SQLite
+  /// Specify testing parameter as true to use in unit tests
   factory NumeroteCore.sql({bool testing = false}) {
     return NumeroteCore._internal(
       adapter: NotesSQLAdapter(testing: testing),
     );
   }
 
+  /// Deletes absolutely everything
   Future<void> nuke() => _adapter.nuke();
 }
