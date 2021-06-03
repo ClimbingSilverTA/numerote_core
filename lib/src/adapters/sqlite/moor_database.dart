ab50ba541f6b654c9e8fde2667db5212ea2f72e5
Jordan Holland <jordan.mark.holland@gmail.com>
2021-06-03 12:10:29 +1000

    Change DB name.

--- lib/src/adapters/sqlite/moor_database.dart
+++ lib/src/adapters/sqlite/moor_database.dart
@@ -13,8 +13,7 @@ part 'moor_database.g.dart';
 LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
-    // TODO: Decide on naming for DB
-    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    final file = File(p.join(dbFolder.path, 'numerote.sqlite'));
     return VmDatabase(file);
   });
 }
